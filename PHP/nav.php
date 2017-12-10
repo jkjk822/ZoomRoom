e9abfa513ae92b7efb8aa03c044ed975417bf706
jkjk822 <jjaco16@u.rochester.edu>
2017-12-09 23:57:32 -0500

    Add simple nav bar

---
+++ PHP/nav.php
@@ -0,0 +1,6 @@
+<nav class="navbar">
+	<div class="navbar-right">
+		<p class="navbar-text">Signed in as <?php echo "{$_COOKIE['loggedIn']}"?></p>
+		<a href="logout.php"><button type="button" class="btn btn-default navbar-btn">Log Out</button></a>
+	</div>
+</nav>
