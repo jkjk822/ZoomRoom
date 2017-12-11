c690f066247097264d5f0e6a901a7e91900f122d
jkjk822 <jjaco16@u.rochester.edu>
2017-12-11 17:53:57 -0500

    Ajax ready

--- PHP/addEvent.php
+++ PHP/addEvent.php
@@ -5,15 +5,17 @@
 	# main
 	if(!empty($_POST['name']))
 	{
-		addEvent();
+		echo json_encode(addEvent());
 	}
 
 	function addEvent(){
+		$data = array();
+		$errors = array();
 
 		require_once('db_setup.php');
 		$sql = "USE jjaco16;";
 		if ($conn->query($sql) !== TRUE) {
-		   die("Error using  database: " . $conn->error);
+		   databaseError($conn->error);
 		}
 
 		$eventName = get_post($conn, 'name');
@@ -22,25 +24,57 @@
 		$description = get_post($conn, 'desc');
 		$startTime = get_post($conn, 'start');
 		$endTime = get_post($conn, 'end');
+		
+		if(!isValid($conn, $location){
+			$errors['room'] = "Invalid room";
+			$data['errors'] = $errors;
+			return $data;
+		}
+	
 
 		# set up query and post it to database
 		$stmt = $conn->prepare("INSERT INTO Event VALUES(0, ?, ?, ?, ?, STR_TO_DATE(?, '%Y-%m-%dT%H:%i'), STR_TO_DATE(?, '%Y-%m-%dT%H:%i'), 'Other');");
- 		if(!$stmt) die("Error: " . $conn->error);
+ 		if(!$stmt) databaseError($conn->error);
 		$stmt->bind_param("ssssss", $eventName, $host, $location, $description, $startTime, $endTime);
 		$stmt->execute();
 
-		if ($stmt) {
-			echo "New event created successfully";
-		} else{
-			die("Error: " . $conn->error);
+		if (!$stmt) {
+			databaseError($conn->error);
 		}
-
+		$stmt->close();
 		$conn->close();
+
+		$data['message'] = "New event created successfully";
+		return $data;
 	}
+
+    function databaseError($error){
+        $errors['database'] = $error;
+        $data['errors'] = $errors;
+        return $data;
+    }
 
 	function get_post($database, $var){
 		return $database->real_escape_string($_POST[$var]);
 	}
+
+	# Check for valid room
+    function isValid($database, $room){
+
+        # set up query and post it to database
+        $stmt = $database->prepare("SELECT roomID FROM userInfo WHERE roomID = ?");
+        if(!$stmt) databaseError($database->error);
+        $stmt->bind_param("s", $room);
+        $stmt->execute();
+        $stmt->store_result();
+        # found room
+        if($stmt->num_rows > 0){
+            $stmt->close();
+            return true;
+        }
+        $stmt->close();
+        return false;
+    }
 
 ?>
 
