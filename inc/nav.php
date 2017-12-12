1a4a8b9ce727fe708f6b524cc4fc6285338517c9
River <jkjk822@yahoo.com.au>
2017-12-12 12:19:42 -0500

    Navbar setup

--- inc/nav.php
+++ inc/nav.php
@@ -1,6 +1,10 @@
 <nav class="navbar">
 	<div class="navbar-right">
+		<?php if($_COOKIE["loggedIn"]){ ?>
 		<p class="navbar-text">Signed in as <?php echo "{$_COOKIE['loggedIn']}"?></p>
 		<a href="logout.php"><button type="button" class="btn btn-default navbar-btn">Log Out</button></a>
+		<?php } else{ ?>
+		<a href="login.php">Login Here</a>
+		<?php } ?>
 	</div>
 </nav>
