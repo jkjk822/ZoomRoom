9b4b4bb95613f7cdbf204f22554ce8061f482b4f
River <jkjk822@yahoo.com.au>
2017-12-11 22:25:57 -0500

    Show all events

---
+++ view-events.php
@@ -0,0 +1,69 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<link rel="stylesheet" href="css/styles.css">
+	<meta charset="utf-8">
+	<title>Zoom Room - View Events</title>
+</head>
+
+<body>
+	<h2>All Events</h2>
+
+<?php
+	require_once('db_setup.php');
+	$sql = "USE jjaco16;";
+	if ($conn->query($sql) !== TRUE) {
+	   echo "Error using  database: " . $conn->error;
+	}
+	// Query:
+	$sql = "SELECT * FROM Event";
+	$result = $conn->query($sql);
+	if($result->num_rows > 0){
+?>
+
+	<table>
+		<tr>
+			<th>Event Name</th>
+			<th>Date &amp; Time</th>
+			<th>Room</th>
+			<th>Host</th>
+			<th>Description</th>
+		</tr>
+<?php
+	while($row = $result->fetch_assoc()){
+?>
+    	<tr>
+	    	<td><?php echo $row['eventID']?></td>
+			<td><?php echo $row['eventName']?></td>
+			<td><?php echo $row['host']?></td>
+			<td><?php echo $row['location']?></td>
+			<td><?php echo $row['description']?></td>
+			<td><?php echo $row['startTime']?></td>
+			<td><?php echo $row['endTime']?></td>
+			<td><?php echo $row['type']?></td>
+      	</tr>
+
+<?php
+	}
+	}
+	else {
+		echo "Nothing to display";
+	}
+?>
+    </table>
+<?php
+	$conn->close();
+?>
+
+	<!-- <form action="login.html" method="POST">
+		<input type="submit" value="Log Out" class="button">
+	</form> -->
+
+	<footer>
+		<p>P1M4 by Johnny Jacobs (8) and Mcvvina Lin (22)</p>
+		<p>CSC 261 Fall 2017</p>
+	</footer>
+</body>
+</html>
