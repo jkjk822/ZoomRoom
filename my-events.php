a5542a5acf8025519de2d378c75b044515e4fec4
River <jkjk822@yahoo.com.au>
2017-12-13 15:41:05 -0500

    Populate my events

--- my-events.php
+++ my-events.php
@@ -1,5 +1,31 @@
 <!doctype html>
 
+<?php	
+	# Redirect to login page if not logged in
+	if(!$_COOKIE['loggedIn']){
+		# Redirect browser
+		header('Location: unauthorized.html'); 
+		exit();
+	}
+
+	require_once('db_setup.php');
+    $sql = "USE jjaco16;";
+    if ($conn->query($sql) !== TRUE) die("Error using  database: " . $conn->error);
+	
+	# set up query and post it to database
+	$stmt = $conn->prepare("SELECT * FROM Event WHERE host = ?;");
+	if(!$stmt) die("Error: " . $conn->error);
+	$stmt->bind_param("s", $_COOKIE['loggedIn']);
+	$stmt->execute();
+
+	#store result
+	$result = $stmt->get_result();
+
+	# cleanup
+	$stmt->close();
+	$conn->close();
+?>
+
 <html lang="en">
 
 <head>
@@ -11,27 +37,36 @@
 <body>
 	<?php include 'inc/nav.php'; ?>
 	<h2>My Events</h2>
-
-	<table>
-		<tr>
-			<th>Event Name</th>
-			<th>Starts</th>
-			<th>Ends</th>
-			<th>Room</th>
-			<th>Host</th>
-			<th>Description</th>
-			<th>Type</th>
-		</tr>
-		<tr>
-			<td><a href="manage-event.php">Event 1</a></td>
-			<td>Start time</td>
-			<td>End time</td>
-			<td>Room 2</td>
-			<td>Host 3</td>
-			<td>Description 4</td>
-			<td>Type 5</td>
-		</tr>
-	</table>
+<?php
+	if($result->num_rows > 0){
+?>
+		<table>
+			<tr>
+				<th>Event Name</th>
+				<th>Starts</th>
+				<th>Ends</th>
+				<th>Room</th>
+				<th>Description</th>
+				<th>Type</th>
+			</tr>
+	<?php
+		while($row = $result->fetch_assoc()){
+	?>
+	      <tr>
+	        <td><a href="manage-event.php/?event=<?php echo htmlspecialchars($row['eventID']) ?>"><?php echo $row['eventName']?></td>
+	        <td><?php echo $row['startTime']?></td>
+	        <td><?php echo $row['endTime']?></td>
+	        <td><?php echo $row['location']?></td>
+	        <td><?php echo $row['description']?></td>
+	        <td><?php echo $row['type']?></td>
+	      </tr>
+	<?php
+		}
+	?>
+		</table>
+<?php		
+	}
+?>
 
 	<footer>
 		<p>P1M4 by Johnny Jacobs (8) and Mcvvina Lin (22)</p>
