6dd2d0015e3af7e839673f78c139859dd55f22b6
mcvvina <mcvvina.lin@gmail.com>
2017-12-13 20:50:58 -0500

    restyled nav bar

--- inc/nav.php
+++ inc/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar">
 	<ul class="navbar-right">
 		<?php if($_COOKIE["loggedIn"]){ ?>
-		<p class="navbar-text">Signed in as <?php echo "{$_COOKIE['loggedIn']}"?></p>
+		<a class="navbar-text">Signed in as <?php echo "{$_COOKIE['loggedIn']}"?></a>
 		<a href="logout.php"><button type="button" class="button">Log Out</button></a>
 		<?php } else{ ?>
 		<a href="login.php"><button type="button" class="button">Login</button></a>
