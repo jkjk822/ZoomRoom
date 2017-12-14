b0b12a797f91fc4435f8b49798a7388203b5d87e
mcvvina <mcvvina.lin@gmail.com>
2017-12-13 21:38:40 -0500

    nav bar update

--- inc/nav.php
+++ inc/nav.php
@@ -4,6 +4,7 @@
 		<a class="navbar-text">Signed in as <?php echo "{$_COOKIE['loggedIn']}"?></a>
 		<a href="logout.php"><button type="button" class="button">Log Out</button></a>
 		<?php } else{ ?>
+		<a class="navbar-text">You are not signed in.</a>
 		<a href="login.php"><button type="button" class="button">Login</button></a>
 		<?php } ?>
 	</ul>
